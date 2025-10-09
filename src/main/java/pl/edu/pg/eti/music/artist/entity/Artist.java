df7040cf0d22e5fde57b8863672606fefe0f8ba4
piotreuszxx <130872389+piotreuszxx@users.noreply.github.com>
2025-10-09 17:25:14 +0200

    debutYear as localdate

--- src/main/java/pl/edu/pg/eti/music/artist/entity/Artist.java
+++ src/main/java/pl/edu/pg/eti/music/artist/entity/Artist.java
@@ -2,12 +2,13 @@ package pl.edu.pg.eti.music.artist.entity;
 
 import pl.edu.pg.eti.music.song.entity.Song;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Artist {
     String name;
     String country;
-    int debutYear;
+    LocalDate debutYear;
     double weight;
     List<Song> songs;
 }
--- src/main/java/pl/edu/pg/eti/music/song/entity/Song.java
+++ src/main/java/pl/edu/pg/eti/music/song/entity/Song.java
@@ -2,10 +2,12 @@ package pl.edu.pg.eti.music.song.entity;
 
 import pl.edu.pg.eti.music.artist.entity.Artist;
 
+import java.time.LocalDate;
+
 public class Song {
     String title;
     String genre;
-    int releaseYear;
+    LocalDate releaseYear;
     double duration; // eg. 2.46
 
     Artist artist;
