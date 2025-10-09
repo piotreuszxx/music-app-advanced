0d2285b131ea222866d6d00d52ec8a34ea888032
piotreuszxx <130872389+piotreuszxx@users.noreply.github.com>
2025-10-09 17:50:26 +0200

    add getGenreName method

--- src/main/java/pl/edu/pg/eti/music/song/entity/Genre.java
+++ src/main/java/pl/edu/pg/eti/music/song/entity/Genre.java
@@ -3,7 +3,7 @@ package pl.edu.pg.eti.music.song.entity;
 public enum Genre {
     HIPHOP, CLASSICAL, ROCK, POP, METAL;
 
-    String getRoleName() {
+    String getGenreName() {
         if (this == HIPHOP) {
             return "hiphop";
         } else if (this == ROCK){
